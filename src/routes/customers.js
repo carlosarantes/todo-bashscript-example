1d3c1e1efce066109010dbd12d2e5a8d4a2ec6fc
Carlos Augusto Arantes <carl.systemsbr@gmail.com>
2023-03-27 22:54:17 +0100

    a

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -4,6 +4,7 @@ const customerRoutes = express.Router()
 customerRoutes.get('/', (req, res) => {
     res.send('Getting all customers from database.')
 })
+// teste
 
 
 customerRoutes.get('/:id', (req, res) => {
